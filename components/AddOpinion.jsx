804c0906800f099b287718f5a9ca1604732c51a4
Paulina Kaczo <p-cioch@wp.pl>
2023-05-11 11:41:32 +0200

    add styles scss to AddOpinion component

--- components/AddOpinion.jsx
+++ components/AddOpinion.jsx
@@ -1,4 +1,6 @@
-import {useState} from "react";
+import React, {useState} from "react";
+import {faStar} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 function AddOpinion() {
     const [showInput, setShowInput] = useState(false);
@@ -14,16 +16,16 @@ function AddOpinion() {
     }
 
     return (
-        <div>
-            <button onClick={handleAddOpinion}> Add your opinion</button>
+        <div className='opinion'>
+            <button className='btn opinion_btn' onClick={handleAddOpinion}> Add your opinion</button>
             {showInput && (
-                <form>
+                <form className='opinion_form'>
                     <label htmlFor="opinion"> Opinion:</label>
                     <input type="number" min='1' max='10' onChange={(e) => {setOpinion(e.target.value)}}/>
-                        <button onClick ={handleOpinionSubmit}>Add</button>
+                        <button className='btn add_btn' onClick ={handleOpinionSubmit}>Add</button>
                 </form>
             )}
-            {opinion && <p>Your opinion: {opinion}</p>}
+            {opinion && <p>Your opinion: {opinion} <FontAwesomeIcon className="star_icon" icon={faStar}/></p>}
         </div>
     );
 }
