94b1896d5300fd2197eaf6453375ed2c7087e1d0
Paulina Kaczo <p-cioch@wp.pl>
2023-05-11 15:04:24 +0200

    prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,37 @@
-import '../scss/main.scss'
+import "../scss/main.scss";
 import SearchMovies from "../components/SearchMovies.jsx";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginOrRegisterForm from "../components/LoginOrRegisterForm.jsx";
 import MovieDetails from "../components/MovieDetails.jsx";
 import TvDetails from "../components/TvDetails.jsx";
 
-
-
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <LoginOrRegisterForm/>
-    },
-    {
-        path: "/home",
-        element: <SearchMovies/>
-    },
-    {
-        path: "/movie/:movieId",
-        element: <MovieDetails/>
-    },
-    {
-        path: "/tv/:tvId",
-        element: <TvDetails/>
-    },
-
+  {
+    path: "/",
+    element: <LoginOrRegisterForm />,
+  },
+  {
+    path: "/home",
+    element: <SearchMovies />,
+  },
+  {
+    path: "/movie/:movieId",
+    element: <MovieDetails />,
+  },
+  {
+    path: "/tv/:tvId",
+    element: <TvDetails />,
+  },
 ]);
 
 function App() {
-
-    return (
-        <>
-
-            <div className="App">
-                <RouterProvider router={router}/>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="App">
+        <RouterProvider router={router} />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
